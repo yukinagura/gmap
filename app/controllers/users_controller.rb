ad2f7325e2b2863b35d62bfe04df45cad80e248e
unknown <rasira867@gmail.com>
2015-09-02 13:50:36 +0900

    second finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,16 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
+    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+    marker.lat user.latitude
+    marker.lng user.longitude
+    marker.infowindow user.description
+    marker.picture({
+          "url" => "/assets/doubutu1.png",
+          "width" =>  36,
+          "height" => 36
+        })
+   end
   end
 
   # GET /users/1
